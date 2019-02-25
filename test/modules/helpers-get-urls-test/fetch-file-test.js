f7775fee22854444f2d9307827038d8fe5a28798
hopokuan <hopokuan@umich.edu>
2019-02-25 00:04:08 -0500

    Updated fetch-file-test

--- test/modules/helpers-get-urls-test/fetch-file-test.js
+++ test/modules/helpers-get-urls-test/fetch-file-test.js
@@ -1,11 +1,11 @@
 const test = require('ava')
 const fetchFile = require('../../../lib/helpers-get-urls/fetch-file')
 const { arraysAreEqual } = require('../../../lib/test-helpers')
+const expectedOutput = ['https://alheimsins.net/',
+  'https://alheimsins.net/projects/',
+  'https://alheimsins.net/changes/']
 
 test('existing file can be fetched', async (t) => {
-  var expectedOutput = ['https://alheimsins.net/',
-    'https://alheimsins.net/projects/',
-    'https://alheimsins.net/changes/']
   var urlsFromJson = await (fetchFile('https://raw.githubusercontent.com/p1ho/site-validator-cli/master/test/data/urls.json'))
   t.is(true, arraysAreEqual(urlsFromJson, expectedOutput))
   var urlsFromTxt = await (fetchFile('https://raw.githubusercontent.com/p1ho/site-validator-cli/master/test/data/urls.txt'))
