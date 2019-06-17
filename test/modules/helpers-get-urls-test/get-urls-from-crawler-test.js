abd8359a6104ec2ba0e84538aae5429d2784c142
hopokuan <hopokuan@umich.edu>
2019-06-17 17:12:35 -0400

    Updated test sitemap

--- test/modules/helpers-get-urls-test/get-urls-from-crawler-test.js
+++ test/modules/helpers-get-urls-test/get-urls-from-crawler-test.js
@@ -3,11 +3,16 @@ const getUrlsFromCrawler = require('../../../lib/helpers-get-urls/get-urls-from-
 const expectedOutput = [
   'https://p1ho.github.io/site-validator-cli/test/data/site/index.html',
   'https://p1ho.github.io/site-validator-cli/test/data/site/page.html',
-  'https://p1ho.github.io/site-validator-cli/test/data/site/from_page.html']
+  'https://p1ho.github.io/site-validator-cli/test/data/site/from_page.html',
+  'https://p1ho.github.io/site-validator-cli/test/data/site/page_warning.html',
+  'https://p1ho.github.io/site-validator-cli/test/data/site/page_fail.html'
+]
 const unreferenced = 'https://p1ho.github.io/site-validator-cli/test/data/site/unreferenced.html'
 
 test('urls are found (cache/debug set to false)', async (t) => {
   var urls = await getUrlsFromCrawler('https://p1ho.github.io/site-validator-cli/test/data/site/index.html', false, false)
-  t.deepEqual(urls, expectedOutput)
+  urls.forEach(url => {
+    t.true(expectedOutput.includes(url))
+  })
   t.not(true, urls.includes(unreferenced))
 })
