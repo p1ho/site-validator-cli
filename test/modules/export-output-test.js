bd439c59f1184d33ede7c99acbe03f404f72ae9a
hopokuan <hopokuan@umich.edu>
2019-06-22 03:28:13 -0400

    Added tests for export-output.js

---
+++ test/modules/export-output-test.js
@@ -0,0 +1,17 @@
+const test = require('ava')
+const fs = require('fs')
+const exportOutput = require('../../lib/export-output')
+
+test('test output can be created and saved', t => {
+  var results = []
+  var options = {
+    quiet: false,
+    singlePage: false,
+    output: 'test-url',
+    path: 'test-url'
+  }
+  exportOutput(results, options)
+  var pwd = process.cwd().replace(/\\/g, '/')
+  t.true(fs.existsSync(`${pwd}/test-url.json`))
+  fs.unlinkSync(`${pwd}/test-url.json`)
+})
